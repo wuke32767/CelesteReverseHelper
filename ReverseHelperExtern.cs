2b2c1e6cb7c68bae41ee72371c820e60f2788178
wuke32767 <2446214230@qq.com>
2024-03-12 16:10:27 +0800

    move type

--- ReverseHelperExtern.cs
+++ ReverseHelperExtern.cs
@@ -355,33 +355,6 @@ namespace Celeste.Mod.ReverseHelper
         //public static string[] debugger;//what if ReverseHelper is named as ConverseHelper in assembly?
         //public static Assembly[] debugger_;
 
-
-        [ModExportName("ReverseHelper.DreamBlock")]
-        public static class Interop
-        {
-            /// <summary>
-            /// if a entity acts like DreamBlock, use this.
-            /// it should access Inventory.DreamDash only when awaking, and save it to a field.
-            /// then, it controls visuals with that field and [De]ActivateNoRoutine. 
-            /// and NO game logic relys on Inventory.DreamDash and that field.
-            /// </summary>
-            public static void RegisterDreamBlockLike(Type targetType, Action<Entity> ActivateNoRoutine, Action<Entity> DeactivateNoRoutine)
-            {
-                if(targetType is not null&&ActivateNoRoutine is not null&&DeactivateNoRoutine is not null)
-                {
-                    ReversedDreamBlock.ExternalDreamBlockLike[targetType] = (ActivateNoRoutine, DeactivateNoRoutine);
-                }
-            }
-            /// <summary>
-            /// or, use this to check if your entity is enabled.
-            /// notice that because of awake order, better to awake at the end of awake frame.
-            /// </summary>
-            /// <param name="e">the entity to be checked.</param>
-            public static bool PlayerHasDreamDash(Entity e)
-            {
-                return ReversedDreamBlock.dreamblock_enabled(e);
-            }
-        }
         public static void Load()
         {
             typeof(Interop).ModInterop();
---
+++ ReverseHelperInterop.cs
@@ -0,0 +1,35 @@
+﻿using Celeste.Mod.ReverseHelper.Entities;
+using Monocle;
+using MonoMod.ModInterop;
+using System;
+
+namespace Celeste.Mod.ReverseHelper
+{
+    [ModExportName("ReverseHelper.DreamBlock")]
+    public static class Interop
+    {
+        /// <summary>
+        /// if a entity acts like DreamBlock, use this.
+        /// it should access Inventory.DreamDash only when awaking, and save it to a field.
+        /// then, it controls visuals with that field and [De]ActivateNoRoutine. 
+        /// and NO game logic relys on Inventory.DreamDash and that field.
+        /// </summary>
+        public static void RegisterDreamBlockLike(Type targetType, Action<Entity> ActivateNoRoutine, Action<Entity> DeactivateNoRoutine)
+        {
+            if (targetType is not null && ActivateNoRoutine is not null && DeactivateNoRoutine is not null)
+            {
+                ReversedDreamBlock.ExternalDreamBlockLike[targetType] = (ActivateNoRoutine, DeactivateNoRoutine);
+            }
+        }
+        /// <summary>
+        /// or, use this to check if your entity is enabled.
+        /// notice that because of awake order, better to awake at the end of awake frame.
+        /// </summary>
+        /// <param name="e">the entity to be checked.</param>
+        public static bool PlayerHasDreamDash(Entity e)
+        {
+            return ReversedDreamBlock.dreamblock_enabled(e);
+        }
+    }
+
+}
